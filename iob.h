9edd283d82847b5e3b7ab31163d30720ac001530
Sam Lavers <sam@lave.rs>
2014-04-27 13:17:54 +0100

    Renamed IO_ constants to IOB_ for consistency

--- iob.h
+++ iob.h
@@ -1,20 +1,20 @@
 
-#define IO_CENTER 		0
-#define IO_LEFT			1
-#define IO_UP 			2
-#define IO_RIGHT 		3
-#define IO_DOWN			4
-#define IO_DIRECTION	5
-#define IO_ANY 			6
+#define IOB_CENTER 		0
+#define IOB_LEFT			1
+#define IOB_UP 			2
+#define IOB_RIGHT 		3
+#define IOB_DOWN			4
+#define IOB_DIRECTION	5
+#define IOB_ANY 			6
 
-#define IO_RED_ON 		PORTB |= _BV(PB7);
-#define IO_RED_OFF		PORTB &= ~_BV(PB7);
+#define IOB_RED_ON 		PORTB |= _BV(PB7);
+#define IOB_RED_OFF		PORTB &= ~_BV(PB7);
 
-#define IO_YELLOW_ON 	PORTD |= _BV(PD4);
-#define IO_YELLOW_OFF 	PORTD &= ~_BV(PD4);
+#define IOB_YELLOW_ON 	PORTD |= _BV(PD4);
+#define IOB_YELLOW_OFF 	PORTD &= ~_BV(PD4);
 
-#define IO_GREEN_ON 	PORTD |= _BV(PD6);
-#define IO_GREEN_OFF 	PORTD &= ~_BV(PD6);
+#define IOB_GREEN_ON 	PORTD |= _BV(PD6);
+#define IOB_GREEN_OFF 	PORTD &= ~_BV(PD6);
 
 void iob_init();
 
